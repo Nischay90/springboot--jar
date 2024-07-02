21efb3c688447214805c003a9ddd71b8ccf56c8b
Nischay90 <nischayhosanna90@gmail.com>
2024-07-02 14:46:25 +0530

    first commit

---
+++ src/main/java/com/jar/maven_tomcat/demo_jar_maven_tomcat/DemoJarMavenTomcatApplication.java
@@ -0,0 +1,13 @@
+package com.jar.maven_tomcat.demo_jar_maven_tomcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJarMavenTomcatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJarMavenTomcatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jar/maven_tomcat/demo_jar_maven_tomcat/DemoJarMavenTomcatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jar.maven_tomcat.demo_jar_maven_tomcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJarMavenTomcatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
